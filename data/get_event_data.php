1c44b02bc48ddae6d0ed1a5c7627184b8bf20e81
Lee <jessy1092@gmail.com>
2014-04-19 15:09:56 +0800

    Web init

---
+++ data/get_event_data.php
@@ -0,0 +1,31 @@
+{
+	"eid":1,
+	"date":{"full":"2014-03-18","yyyy":2014,"mm":3,"dd":18},
+	"title":"事件一",
+	"why":"事件起因",
+	"place":"地點一",
+	"images":[
+	{ "id":1,"title":"圖片一", "url":"image/1.jpg", "description":"圖片說明 1" },
+	{ "id":2,"title":"圖片二", "url":"image/2.jpg", "description":"圖片說明 2" },
+	{ "id":3,"title":"圖片三", "url":"image/3.jpg", "description":"圖片說明 3" },
+	{ "id":4,"title":"圖片四", "url":"image/4.jpg", "description":"圖片說明 4" },
+	{ "id":5,"title":"圖片五", "url":"image/5.jpg", "description":"圖片說明 5" }],
+	"news":[
+	{ "id":1,"title":"新聞一", "url":"news/1.html" },
+	{ "id":2,"title":"新聞二", "url":"news/2.html" },
+	{ "id":3,"title":"新聞三", "url":"news/3.html" },
+	{ "id":4,"title":"新聞四", "url":"news/4.html" },
+	{ "id":5,"title":"新聞五", "url":"news/5.html" }],
+	"comment":[
+	{ "id":1,"title":"討論一", "url":"comment/1.html" },
+	{ "id":2,"title":"討論二", "url":"comment/2.html" },
+	{ "id":3,"title":"討論三", "url":"comment/3.html" },
+	{ "id":4,"title":"討論四", "url":"comment/4.html" },
+	{ "id":5,"title":"討論五", "url":"comment/5.html" }],
+	"media":[
+	{ "id":1,"title":"影音一", "url":"media/1.mp4" },
+	{ "id":2,"title":"影音二", "url":"media/2.mp4" },
+	{ "id":3,"title":"影音三", "url":"media/3.mp4" },
+	{ "id":4,"title":"影音四", "url":"media/4.mp4" },
+	{ "id":5,"title":"影音五", "url":"media/5.mp4" }]
+}
